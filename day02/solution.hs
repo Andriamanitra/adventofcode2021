93f31d049a0e62683db82078f63d12ad817aa018
Andriamanitra <mikko77792@gmail.com>
2021-12-02 10:30:33 +0200

    improve(?) haskell solution

--- day02/solution.hs
+++ day02/solution.hs
@@ -1,32 +1,36 @@
 import System.IO
+import Data.Char (toUpper)
 
-solvePart1 :: [[String]] -> Int
+data Instruction = Forward Int | Up Int | Down Int deriving (Read)
+
+readInstruction :: String -> Instruction
+readInstruction (x:xs) = read capitalized where capitalized = toUpper x : xs
+
+solvePart1 :: [Instruction] -> Int
 solvePart1 input = solve (0, 0) input
     where
         solve (x, y) [] = x * y
-        solve (x, y) ([instruction, num]:rest) = solve nextPos rest
-            where
-                n = read num :: Int
-                move "forward" = (x + n, y)
-                move "up"      = (x, y - n)
-                move "down"    = (x, y + n)
-                nextPos = move instruction
+        solve (x, y) (instruction:rest) =
+            let nextPos = case instruction of
+                    Forward n -> (x + n, y)
+                    Up n      -> (x, y - n)
+                    Down n    -> (x, y + n)
+            in solve nextPos rest
 
-solvePart2 :: [[String]] -> Int
+solvePart2 :: [Instruction] -> Int
 solvePart2 input = solve (0, 0, 0) input
     where
         solve (x, y, aim) [] = x * y
-        solve (x, y, aim) ([instruction, num]:rest) = solve nextPos rest
-            where
-                n = read num :: Int
-                move "forward" = (x + n, y + n * aim, aim)
-                move "up"      = (x, y, aim - n)
-                move "down"    = (x, y, aim + n)
-                nextPos = move instruction
+        solve (x, y, aim) (instruction:rest) =
+            let nextPos = case instruction of
+                    Forward n -> (x + n, y + n * aim, aim)
+                    Up n      -> (x, y, aim - n)
+                    Down n    -> (x, y, aim + n)
+            in solve nextPos rest
 
 main :: IO()
 main = do
     contents <- openFile "input.txt" ReadMode >>= hGetContents
-    let input = map words $ lines contents
+    let input = map readInstruction $ lines contents
     print $ solvePart1 input
     print $ solvePart2 input
