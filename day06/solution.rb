279f6b490d3341e7796ad527c518cd9cfc57c235
Andriamanitra <mikko77792@gmail.com>
2021-12-06 07:30:32 +0200

    use .times

--- day06/solution.rb
+++ day06/solution.rb
@@ -1,11 +1,11 @@
-input = File.open("input.txt").read.split(",").map(&:to_i)
+input = File.open('input.txt').read.split(',').map(&:to_i)
 
 fish = [0] * 9
 input.each do |age|
     fish[age] += 1
 end
 
-(1..256).each do |i|
-    puts fish.sum if i == 80 || i == 256
+256.times do |i|
     fish[(i + 7) % 9] += fish[i % 9]
+    puts fish.sum if i == 79 || i == 255
 end
