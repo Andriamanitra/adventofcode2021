7e2f8b987ba4dded0a9642ba4876546ff2bc87ed
Andriamanitra <mikko77792@gmail.com>
2021-12-11 08:14:57 +0200

    get rid of rest of magic numbers

--- day11/solution.rb
+++ day11/solution.rb
@@ -1,10 +1,13 @@
-POWER_TO_FLASH = 10
+ENERGY_REQUIRED_TO_FLASH = 10
+ENERGY_GAIN_PER_STEP = 1
 FLASH_RANGE = 1
 FLASH_STRENGTH = 1
 
 input = File.open("input.txt").read.split.map{_1.chars.map(&:to_i)}
 ROWS = input.size
+MAXROW = ROWS - 1
 COLS = input[0].size
+MAXCOL = COLS - 1
 input.flatten!
 
 flashes = 0
@@ -12,17 +15,17 @@ part1 = nil
 part2 = nil
 (1..).find { |iteration|
     flashed = input.map { false }
-    input.map! { _1 + 1 }
-    while input.zip(flashed).any? { |v,f| v >= POWER_TO_FLASH && !f }
+    input.map! { _1 + ENERGY_GAIN_PER_STEP }
+    while input.zip(flashed).any? { |v,f| v >= ENERGY_REQUIRED_TO_FLASH && !f }
         input.each_index do |i|
-            next if input[i] < POWER_TO_FLASH || flashed[i]
+            next if input[i] < ENERGY_REQUIRED_TO_FLASH || flashed[i]
             flashes += 1
             flashed[i] = true
             row, col = i.divmod(COLS)
             row_from = [0, row - FLASH_RANGE].max
-            row_to = [ROWS - 1, row + FLASH_RANGE].min
+            row_to = [MAXROW, row + FLASH_RANGE].min
             col_from = [0, col - FLASH_RANGE].max
-            col_to = [COLS - 1, col + FLASH_RANGE].min
+            col_to = [MAXCOL, col + FLASH_RANGE].min
             (col_from..col_to).each do |col|
                 (row_from..row_to).each do |row|
                     input[row * COLS + col] += FLASH_STRENGTH
@@ -30,7 +33,7 @@ part2 = nil
             end
         end
     end
-    input.map!{_1 > 9 ? 0 : _1}
+    input.map!{_1 >= ENERGY_REQUIRED_TO_FLASH ? 0 : _1}
     part1 = flashes if part1.nil? && iteration == 100
     part2 = iteration if part2.nil? && flashed.all?
     part1 && part2
