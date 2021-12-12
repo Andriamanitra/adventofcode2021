9ca9586667d71c200de087060ef85e30e65e627e
Andriamanitra <mikko77792@gmail.com>
2021-12-12 07:33:48 +0200

    actually part2 solution works for part1 too

--- day12/solution.rb
+++ day12/solution.rb
@@ -10,27 +10,19 @@ end
 
 def small?(cave) = cave.match?(/^[a-z]+/)
 
-def countpaths(graph, visited, from, to)
-    return 1 if from == to
-    return 0 if visited.include?(from)
-    graph[from].sum do |dest|
-        countpaths(graph, visited + (small?(from) ? [from] : []), dest, to)
-    end
-end
-
-def countpaths2(graph, doubled, visited, from, to)
+def countpaths(graph, doubled, visited, from, to)
     return 1 if from == to
     if visited.include?(from)
         return 0 if doubled || from == 'start'
         doubled = true
     end
     graph[from].sum do |dest|
-        countpaths2(graph, doubled, small?(from) ? visited + [from] : visited, dest, to)
+        countpaths(graph, doubled, small?(from) ? visited + [from] : visited, dest, to)
     end
 end
 
 # Part 1
-puts countpaths(cavemap, [], 'start', 'end')
+puts countpaths(cavemap, true, [], 'start', 'end')
 
 # Part 2
-puts countpaths2(cavemap, false, [], 'start', 'end')
+puts countpaths(cavemap, false, [], 'start', 'end')
