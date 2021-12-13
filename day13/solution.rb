c746cb6e848a3ad3fd836182cb6b50e093501185
Andriamanitra <mikko77792@gmail.com>
2021-12-13 08:31:07 +0200

    solve day 13

---
+++ day13/solution.rb
@@ -0,0 +1,56 @@
+input = File.open('input.txt').read
+points, instructions = input.split("\n\n")
+points = points.split.map { |line| line.split(',').map(&:to_i) }
+
+class Paper
+  attr_reader :points
+
+  def initialize(points)
+    @points = points
+    @width  = points.map { |x, _y| x }.max + 1
+    @height = points.map { |_x, y| y }.max + 1
+  end
+
+  def foldX(fold)
+    @points = @points.map do |x, y|
+      x = fold - (x - fold) if x > fold
+      [x, y]
+    end
+    @points = @points.uniq
+    @width = fold
+  end
+
+  def foldY(fold)
+    @points = @points.map do |x, y|
+      y = fold - (y - fold) if y > fold
+      [x, y]
+    end
+    @points = @points.uniq
+    @height = fold
+  end
+
+  def to_s
+    canvas = Array.new(@height) { ' ' * @width }
+    @points.each do |x, y|
+      canvas[y][x] = '█'
+    end
+    canvas.join("\n")
+  end
+end
+
+paper = Paper.new(points)
+
+instructions.each_line.zip(1..) do |instruction, fold_number|
+  raise 'Invalid instruction!' unless instruction[/fold along (x|y)=(\d+)/]
+
+  if $1 == 'x'
+    paper.foldX($2.to_i)
+  elsif $1 == 'y'
+    paper.foldY($2.to_i)
+  end
+  # Part 1
+  puts paper.points.size if fold_number == 1
+end
+
+# Part 2
+puts paper
