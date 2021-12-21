eda308bfd32ff0e2587bba97e7ef05358bb17249
Andriamanitra <mikko77792@gmail.com>
2021-12-21 09:21:27 +0200

    refactor

--- day21/solution.py
+++ day21/solution.py
@@ -11,37 +11,30 @@ def part1dice():
             yield 6 + 9 * i
 
 dice = part1dice()
-score_p1 = 0
-score_p2 = 0
-pos_p1 = p1start
-pos_p2 = p2start
+scores = [0, 0]
+positions = [p1start, p2start]
 rolls = 0
 while True:
+    player = rolls % 2
+    positions[player] = (positions[player] + next(dice) - 1) % 10 + 1
+    scores[player] += positions[player]
     rolls += 1
-    pos_p1 = (pos_p1 + next(dice) - 1) % 10 + 1
-    score_p1 += pos_p1
-    if score_p1 >= 1000: break
-    rolls += 1
-    pos_p2 = (pos_p2 + next(dice) - 1) % 10 + 1
-    score_p2 += pos_p2
-    if score_p2 >= 1000: break
-print(3 * rolls * min(score_p1, score_p2))
+    if max(scores) >= 1000: break
+print(3 * rolls * min(scores))
 
 
 # Part 2
 @cache
-def part2(pos_p1, pos_p2, score_p1, score_p2, t):
-    if t: return tuple(reversed(part2(pos_p2, pos_p1, score_p2, score_p1, not t)))
+def part2(pos_p1, pos_p2, score_p1, score_p2):
     if score_p1 >= 21: return (1, 0)
     if score_p2 >= 21: return (0, 1)
-    a = 0
-    b = 0
+    p1wins, p2wins = 0, 0
     for roll in (d1 + d2 + d3 for d1 in (1,2,3) for d2 in (1,2,3) for d3 in (1,2,3)):
         next_pos = (pos_p1 + roll - 1) % 10 + 1
-        an, bn = part2(next_pos, pos_p2, score_p1 + next_pos, score_p2, not t)
-        a += an
-        b += bn
-    return (a, b)
+        w2, w1 = part2(pos_p2, next_pos, score_p2, score_p1 + next_pos)
+        p1wins += w1
+        p2wins += w2
+    return (p1wins, p2wins)
 
-p1wins, p2wins = part2(p1start, p2start, 0, 0, False)
+p1wins, p2wins = part2(p1start, p2start, 0, 0)
 print(max(p1wins, p2wins))
